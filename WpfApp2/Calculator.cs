e62e384327faf2f3da468d54b745ae1463aa6797
Exilense <Exilense@mail.com>
2022-10-13 22:51:12 +0300

    refactoring

--- WpfApp2/Calculator.cs
+++ WpfApp2/Calculator.cs
@@ -10,78 +10,66 @@ namespace WpfApp2
     {
         public string Starter(string numbers)
         {
-            Calculator pr = new Calculator();
-            string result = pr.Math(numbers);
-            while (true)
-            {
-                float n;
-                if (float.TryParse(result, out n))
-                {
-                    break;
-                }
-                if (!result.Contains('-') && !result.Contains('*') && !result.Contains('/') && !result.Contains('+'))
-                {
-                    break;
-                }
-                result = pr.Math(result);
-            }
+            string result = Math(numbers);
             return result;
         }
         public string Math(string numbers)
         {
-            float[] otveti = new float[100];
-            if(!numbers.Contains('*')&& !numbers.Contains('/')&& !numbers.Contains('+')&& !numbers.Contains('-'))
+
+
+            if (!numbers.Contains('*') && !numbers.Contains('/') && !numbers.Contains('+'))
             {
-                return numbers;
+                if (float.TryParse(numbers, out var temp))
+                {
+                    return numbers;
+                }
             }
+
             if (numbers.Contains('('))
             {
                 numbers = InBrackets(numbers);
-                numbers = Math(numbers);
+
             }
-            for(int i = 0; i < numbers.Length; i++)
-            {
-                bool check = !Char.IsNumber(numbers[i]);
-                if(check == true)
+            if (numbers.Contains('*') || numbers.Contains('/'))
+                for (int i = 0; i < numbers.Length; i++)
                 {
-                    if (i < numbers.Length&&numbers[i] == '*')
+                    bool isSymbol = !Char.IsNumber(numbers[i]);
+                    if (isSymbol)
                     {
-                        numbers = Umnojenie(otveti, numbers.IndexOf('*'), numbers);
-                        numbers= Math(numbers);
-                    }
-                    if (i < numbers.Length&&numbers[i] == '/')
-                    {
-                        numbers = Delenie(otveti, numbers.IndexOf('/'), numbers);
-                        numbers = Math(numbers);
-                    }
-                }
-            }
-            for(int i = 0; i < numbers.Length; i++)
-            {
-                bool check = !Char.IsNumber(numbers[i]);
-                if(check == true)
-                {
-                    if (i < numbers.Length&&numbers[i] == '+' )
-                    {
-                        numbers = Slojenie(otveti, numbers.IndexOf('+'), numbers);
-                        numbers = Math(numbers);
-                    }
-                    if(i < numbers.Length&&numbers[i] == '-' )
-                    {
-                        for (int j = 0; j < numbers.Length; j++)
+                        if (i < numbers.Length && numbers[i] == '*')
                         {
-                            if (!Char.IsNumber(numbers[j]) && numbers[j] != '-' && numbers[j] != ',')
-                            {
-                                break;
-                            }
-                            if (numbers[j] == '-' && j != 0)
-                            {
-                                numbers = Vichitanie(otveti, j, numbers);
-                                numbers = Math(numbers);
-                            }
+                            numbers = MathAction(MathActionType.Mul, i, numbers);
+                            continue;
+                        }
+                        if (i < numbers.Length && numbers[i] == '/')
+                        {
+                            numbers = MathAction(MathActionType.Div, i, numbers);
+                            continue;
                         }
                     }
                 }
+            if (numbers.Contains('+') || numbers.Contains('-'))
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    bool isSymbol = !Char.IsNumber(numbers[i]);
+                    if (isSymbol)
+                    {
+                        if (i < numbers.Length && numbers[i] == '+')
+                        {
+                            numbers = MathAction(MathActionType.Add, i, numbers);
+                            continue;
+                        }
+                        if (i != 0 && numbers[i] == '-')
+                        {
+                            numbers = MathAction(MathActionType.Sub, i, numbers);
+                            continue;
+                        }
+
+                    }
+                }
+            if (numbers.Contains('*') || numbers.Contains('/') || numbers.Contains('+') || numbers.Contains('-'))
+            {
+                numbers = Math(numbers);
             }
             return numbers;
         }
@@ -89,12 +77,12 @@ namespace WpfApp2
         {
             string result = string.Empty;
             int bracketStartIndex = numbers.IndexOf('(');
-            int bracketEndIndex =0;
-            int bracketInCounter=0,bracketOutCounter=0;
-            for(int i = bracketStartIndex; i < numbers.Length;i++)
+            int bracketEndIndex = 0;
+            int bracketInCounter = 0, bracketOutCounter = 0;
+            for (int i = bracketStartIndex; i < numbers.Length; i++)
             {
                 if (numbers[i] == '(') bracketInCounter++;
-                if(numbers[i] == ')')
+                if (numbers[i] == ')')
                 {
                     bracketOutCounter++;
                     if (bracketInCounter == bracketOutCounter)
@@ -104,173 +92,104 @@ namespace WpfApp2
                     }
                 }
             }
-            string expressionInBrackets = numbers.Substring(bracketStartIndex+1, bracketEndIndex-bracketStartIndex-1);
-            result = numbers.Replace(numbers.Substring(bracketStartIndex , bracketEndIndex - bracketStartIndex+1 ), Math(expressionInBrackets));
+            string expressionInBrackets = numbers.Substring(bracketStartIndex + 1, bracketEndIndex - bracketStartIndex - 1);
+            result = numbers.Replace(numbers.Substring(bracketStartIndex, bracketEndIndex - bracketStartIndex + 1), Math(expressionInBrackets));
             return result;
         }
-        public string Skobka(string numbers)
+        public Expression GetExpression(int charID, string numbers)
         {
-            int startSS;
-            int lastSS = 0;
-            string skobochka;
-            int lenth;
-            for(int i = numbers.Length-1; i >= 0; i--)
-            {
-                if(numbers[i] == '(')
-                {
-                    for( int j = i; j < numbers.Length; j++)
-                    {
-                        if(numbers[j] == ')')
-                        {
-                            lastSS = j;
-                            break;
-                        }
-                    }
-                    startSS = i;
-                    lenth = lastSS - startSS;
-                    skobochka = numbers.Substring(startSS + 1, lenth - 1);
-                    float n;
-                    while (true)
-                    {
-                        if (skobochka.Contains('*') || skobochka.Contains('/') || skobochka.Contains('+') || skobochka.Contains('-'))
-                        {
-                            skobochka = Math(skobochka);
-                        }
-                        if (float.TryParse(skobochka, out n))
-                        {
-                            break;
-                        }
-                        if (!skobochka.Contains('*') && !skobochka.Contains('/') && !skobochka.Contains('+') && !skobochka.Contains('-'))
-                        {
-                            break;
-                        }
-                    }
-                    numbers = numbers.Replace(numbers.Substring(startSS, lenth + 1), skobochka);
-                    break;
-                }
-              
-            }
-                return numbers;
-        }
-        public float[] Viroshenie(int charID, string numbers)
-        {
-            string temp = "";
-            string temp1 = "";
-            float indexReplaceStart = 0;
-            float indexReplaceLast = 0;
+
+            string firstNumberString = string.Empty;
+            string secondNumberString = string.Empty;
+            int indexReplaceStart = 0;
+            int indexReplaceLast = 0;
             float number1 = 0;
             float number2 = 0;
-            bool check;
+
             for (int i = charID - 1; i >= 0; i--)
             {
-                check = Char.IsNumber(numbers[i]);
-                if (check == true)
+                var currentChar = numbers[i];
+                var isNumber = Char.IsNumber(currentChar);
+                var isComma = currentChar == ',';
+                var isMinus = currentChar == '-' && i == 0 || currentChar == '-' && !Char.IsNumber(numbers[i - 1]);
+                if (isNumber || isComma || isMinus)
                 {
-                    temp = Char.ToString(numbers[i]);
-                    temp1 = temp + temp1;
+                    firstNumberString += currentChar;
+                    indexReplaceStart = i;
                 }
-                if (numbers[i] == ',')
+
+                else if (i == 0)
                 {
-                    check = true;
-                    temp1 = ',' + temp1;
-                }
-                if (check == false || i == 0)
-                {
-                    if ((numbers[i] == '-' && i == 0) || (numbers[i] == '-' && !Char.IsNumber(numbers[i-1])))
-                    {
-                        temp1 = '-' + temp1;
-                        indexReplaceStart = i;
-                        number1 = float.Parse(temp1);
-                        break;
-                    }
-                indexReplaceStart = i + 1;
-                    if (i == 0)
-                    {
-                        indexReplaceStart = i;
-                    }
-                    number1 = float.Parse(temp1);
+                    indexReplaceStart = i;
+
                     break;
                 }
+                else if (!isNumber)
+                {
+                    indexReplaceStart = i + 1;
+                    break;
+                }
+
             }
-            temp1 = "";
-            if (numbers[charID + 1] == '-')
-            {
-                temp1 = "-";
-                charID = charID + 1;
-            }
+
             for (int i = charID + 1; i < numbers.Length; i++)
             {
-                check = Char.IsNumber(numbers[i]);
-                if (check == true)
+                var currentChar = numbers[i];
+                var isNumber = Char.IsNumber(currentChar);
+                var isComma = currentChar == ',';
+                var isMinus = numbers[i] == '-' && secondNumberString == string.Empty;
+
+                if (isNumber || isComma || isMinus)
                 {
-                    temp = Char.ToString(numbers[i]);
-                    temp1 = temp + temp1;
-                }
-                if (numbers[i] == ',')
-                {
-                    check = true;
-                    temp1 = ',' + temp1;
-                }
-                if (check == false || i == numbers.Length - 1)
-                {
+                    secondNumberString += currentChar;
                     indexReplaceLast = i;
-                    if (i == numbers.Length - 1)
-                    {
-                        indexReplaceLast = i + 1;
-                    }
-                    char[] c = temp1.ToCharArray();
-                    Array.Reverse(c);
-                    temp1 = new string(c);
-                    number2 = float.Parse(temp1);
+                }
+                else if (!isNumber)
+                {
                     break;
                 }
+                else if (i == numbers.Length - 1)
+                {
+                    indexReplaceLast = i;
+                }
+
             }
-            float[] otveti = new float[4];
-            otveti[0] = number1;
-            otveti[1] = number2;
-            otveti[2] = indexReplaceStart;
-            otveti[3] = indexReplaceLast;
-            return otveti;
+            firstNumberString = ReverseString(firstNumberString);
+            number1 = float.Parse(firstNumberString);
+            number2 = float.Parse(secondNumberString);
+            return new Expression(number1, number2, indexReplaceStart, indexReplaceLast);
         }
-        public string Umnojenie(float[] otveti, int charID, string numbers)
+        public string ReverseString(string s)
         {
-            otveti = Viroshenie(charID, numbers);
-            float result = otveti[0] * otveti[1];
-            int lenth = Convert.ToInt32(otveti[3] - otveti[2]);
-            int start = Convert.ToInt32(otveti[2]);
-            string tempString = numbers.Substring(start, lenth);
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        private string MathAction(MathActionType type, int charID, string numbers)
+        {
+            var tempExpression = GetExpression(charID, numbers);
+            int length = tempExpression.EndExpIndex - tempExpression.StartExpIndex + 1;
+            int start = tempExpression.StartExpIndex;
+            float result = 0;
+            switch (type)
+            {
+                case MathActionType.Add: result = tempExpression.FirstNumber + tempExpression.SecondNumber; break;
+                case MathActionType.Sub: result = tempExpression.FirstNumber - tempExpression.SecondNumber; break;
+                case MathActionType.Mul: result = tempExpression.FirstNumber * tempExpression.SecondNumber; break;
+                case MathActionType.Div: result = tempExpression.FirstNumber / tempExpression.SecondNumber; break;
+            }
+
+            string tempString = numbers.Substring(start, length);
             numbers = numbers.Replace(tempString, result.ToString());
             return numbers;
         }
-        public string Delenie(float[] otveti, int charID, string numbers)
-        {
-            otveti = Viroshenie(charID, numbers);
-            float result = otveti[0] / otveti[1];
-            int lenth = Convert.ToInt32(otveti[3] - otveti[2]);
-            int start = Convert.ToInt32(otveti[2]);
-            string tempString = numbers.Substring(start, lenth);
-            numbers = numbers.Replace(tempString, result.ToString());
-            return numbers;
-        }
-        public string Slojenie(float[] otveti, int charID, string numbers)
-        {
-            otveti = Viroshenie(charID, numbers);
-            float result = otveti[0] + otveti[1];
-            int lenth = Convert.ToInt32(otveti[3] - otveti[2]);
-            int start = Convert.ToInt32(otveti[2]);
-            string tempString = numbers.Substring(start, lenth);
-            numbers = numbers.Replace(tempString, result.ToString());
-            return numbers;
-        }
-        public string Vichitanie(float[] otveti, int charID, string numbers)
-        {
-            otveti = Viroshenie(charID, numbers);
-            float result = otveti[0] - otveti[1];
-            int lenth = Convert.ToInt32(otveti[3] - otveti[2]);
-            int start = Convert.ToInt32(otveti[2]);
-            string tempString = numbers.Substring(start, lenth);
-            numbers = numbers.Replace(tempString, result.ToString());
-            return numbers;
-        }
+    }
+    enum MathActionType
+    {
+        Add,
+        Sub,
+        Mul,
+        Div
     }
 }
---
+++ WpfApp2/Expression.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp2
+{
+    internal class Expression
+    {
+        private float firstNumber;
+        private float secondNumber;
+        private int startExpIndex;
+        private int endExpIndex;
+
+        public float FirstNumber { get => firstNumber; }
+        public float SecondNumber { get => secondNumber; }
+        public int StartExpIndex { get => startExpIndex; }
+        public int EndExpIndex { get => endExpIndex; }
+
+        internal Expression(float number1,float number2,int startIndex,int endIndex)
+        {
+            firstNumber = number1;
+            secondNumber = number2;
+            startExpIndex = startIndex;
+            endExpIndex = endIndex;
+        }
+    }
+}
